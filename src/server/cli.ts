10835c5d294069e6136a4475caf9bebbd4777911
mushan0x0 <mushan0x0@gmail.com>
2026-05-11 01:19:42 +0800

    fix: lint format and bump to 0.2.4

--- src/server/cli.ts
+++ src/server/cli.ts
@@ -331,7 +331,11 @@ export async function main(): Promise<void> {
 }
 
 /* c8 ignore start */
-if (process.argv[1]?.endsWith('cli.js') || process.argv[1]?.endsWith('cli.ts') || process.argv[1]?.endsWith('codeproxy')) {
+if (
+  process.argv[1]?.endsWith('cli.js') ||
+  process.argv[1]?.endsWith('cli.ts') ||
+  process.argv[1]?.endsWith('codeproxy')
+) {
   void main();
 }
 /* c8 ignore stop */
